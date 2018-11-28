fdf923363742c6b46b5155643ac17368d0265a9b
madkn <eugeniya.ustinova@gmail.com>
2018-11-28 17:53:04 +0300

    Update pose_calculation.py

--- testing/python/pose_calculation.py
+++ testing/python/pose_calculation.py
@@ -281,3 +281,38 @@ def calculate_pose(oriImg):
     
     return connection_all, candidate, subset, person, all_peaks, heatmap_avg, paf_avg
 
+#generates hitmap from the previously extracted data
+def get_gt_maps_const(subset, points, output_size, radius = 5., initial_size=(128, 64)):
+    if not output_size:
+        hitmaps_gt = np.zeros((18,initial_size[0], initial_size[1]))
+    else:
+        hitmaps_gt = np.zeros((18,output_size[0], output_size[1]))
+
+    # take the person with largest number of points
+    person = max(subset, key=lambda x: x[-1])
+
+    H = output_size[0] if output_size else initial_size[0]
+    W = output_size[1] if output_size else initial_size[1]
+    if output_size:
+        radius = (radius / initial_size[0]  * output_size[0]  +
+                  radius / initial_size[1]  * output_size[1]) / 2
+
+    #print radius
+    for i in xrange(len(person) - 2):
+        if person[i] == -1:
+            continue
+        anchor = points[person[i]][1], points[person[i]][0]
+
+        if output_size:
+            anchor = anchor[0] / initial_size[0] * output_size[0], anchor[1] / initial_size[1] * output_size[1]
+
+        #print anchor, int(round(anchor[0] - radius)), int(round(anchor[0] + radius))
+        for h in xrange(int(round(anchor[0] - radius)), int(round(anchor[0] + radius))+1):
+            for w in xrange(int(round(anchor[1] - radius)), int(round(anchor[1] + radius))+1):
+                if h < 0 or h >= H or w < 0 or w >= W:
+                    continue
+                dist_sq = np.sqrt((h - anchor[0]) ** 2 + (w - anchor[1]) ** 2)
+                if dist_sq <radius+1 :
+                    hitmaps_gt[i, h, w] = 1
+
+    return hitmaps_gt
